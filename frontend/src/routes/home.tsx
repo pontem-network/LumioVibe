5a8095ecf8ed390e7de88943961b3d42659d7b8f
vladimirovmm <vladimirov.m.m@mail.ru>
2025-12-04 12:01:25 +0700

    feat:  settings has been disabled

--- frontend/src/routes/home.tsx
+++ frontend/src/routes/home.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { PrefetchPageLinks } from "react-router";
 import { HomeHeader } from "#/components/features/home/home-header/home-header";
-import { RepoConnector } from "#/components/features/home/repo-connector";
 import { TaskSuggestions } from "#/components/features/home/tasks/task-suggestions";
 import { GitRepository } from "#/types/git";
 import { NewConversation } from "#/components/features/home/new-conversation/new-conversation";
@@ -10,9 +9,7 @@ import { RecentConversations } from "#/components/features/home/recent-conversat
 <PrefetchPageLinks page="/conversations/:conversationId" />;
 
 function HomeScreen() {
-  const [selectedRepo, setSelectedRepo] = React.useState<GitRepository | null>(
-    null,
-  );
+  const [selectedRepo] = React.useState<GitRepository | null>(null);
 
   return (
     <div
@@ -23,10 +20,11 @@ function HomeScreen() {
 
       <div className="pt-[25px] flex justify-center">
         <div
+          id="new_conversation_section"
           className="flex flex-col gap-5 px-6 sm:max-w-full sm:min-w-full md:flex-row lg:px-0 lg:max-w-[703px] lg:min-w-[703px]"
           data-testid="home-screen-new-conversation-section"
         >
-          <RepoConnector onRepoSelection={(repo) => setSelectedRepo(repo)} />
+          {/* <RepoConnector onRepoSelection={(repo) => setSelectedRepo(repo)} /> */}
           <NewConversation />
         </div>
       </div>
