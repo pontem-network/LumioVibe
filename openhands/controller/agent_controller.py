c659783696c9c62cbd270c3ba2e94c798ffe08f6
Maxim Vladimirov <32911557+vladimirovmm@users.noreply.github.com>
2025-12-24 10:52:50 +0700

    feat: Delete containers that have not been started for more than 1 day (#18)
    * feat: Delete containers that have not been started for more than 1 day
    
    * feat: Delete no more than 3 containers per request
    
    * feat: Log errors and continue working when the container is deleted.
    
    * fix: opening vscode in iframe

--- openhands/controller/agent_controller.py
+++ openhands/controller/agent_controller.py
@@ -181,7 +181,7 @@ class AgentController:
             confirmation_mode=confirmation_mode,
         )
 
-        self.state = self.state_tracker.state  # TODO: share between manager and controller for backward compatability; we should ideally move all state related logic to the state manager
+        self.state = self.state_tracker.state  # TODO: share between manager and controller for backward compatibility; we should ideally move all state related logic to the state manager
 
         self.agent_to_llm_config = agent_to_llm_config if agent_to_llm_config else {}
         self.agent_configs = agent_configs if agent_configs else {}
