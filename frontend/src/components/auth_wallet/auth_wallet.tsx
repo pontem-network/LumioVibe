a06c594e236e6be578260882b9641fad6d869c83
vladimirovmm <vladimirov.m.m@mail.ru>
2026-01-06 13:36:04 +0700

    feat: logo

--- frontend/src/components/auth_wallet/auth_wallet.tsx
+++ frontend/src/components/auth_wallet/auth_wallet.tsx
@@ -1,10 +1,7 @@
-import { useTranslation } from "react-i18next";
-import { I18nKey } from "#/i18n/declaration";
 import "./auth_wallet.css";
 import { Logo } from "../features/logo/logo";
 
 export function WalletPage() {
-  const { t } = useTranslation();
   const text = [
     "To access it, you must log in via the Pontem wallet.",
     "To install, visit the ",
@@ -13,14 +10,10 @@ export function WalletPage() {
 
   return (
     <div id="wallet_page">
-      <div className="flex flex-col items-center mb-12">
-        <Logo />
-        <span className="text-[32px] font-bold leading-5 text-white pt-4">
-          {t(I18nKey.LANDING$TITLE)}
-        </span>
-      </div>
-      <p>{text[0]}</p>
+      <Logo />
       <p>
+        {text[0]}
+        <br />
         {text[1]}
         <a href="https://pontem.network/pontem-wallet">{text[2]}</a>
       </p>
--- frontend/src/components/features/logo/logo.tsx
+++ frontend/src/components/features/logo/logo.tsx
@@ -2,10 +2,27 @@ import "./logo.css";
 import bLogo from "#/assets/logo.png";
 
 export function Logo() {
+  const subText = ["CREATE", "WEB3 APPS", "WITH JUST A PROMPT"];
+  const desc = [
+    "TYPE",
+    " a prompt, get production-ready smart contracts instantly. No coding required.",
+  ];
+
   return (
     <div className="main-logo">
-      <img src={bLogo} alt="B" />
-      UILD
+      <div className="logo">
+        <img src={bLogo} alt="B" />
+        UILD
+      </div>
+      <div className="subtext">
+        {subText[0]}
+        <b> {subText[1]} </b>
+        {subText[2]}
+      </div>
+      <div className="desc">
+        <span>{desc[0]}</span>
+        {desc[1]}
+      </div>
     </div>
   );
 }
